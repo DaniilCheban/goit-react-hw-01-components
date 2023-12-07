b71c8f441848771c2aae05c1032c85cb5f0e7d51
DaniilCheban <danyacheban@icloud.com>
2023-12-07 20:35:27 +0100

    TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import { Profile } from './Profile';
 import { Statistics } from './Statistics';
 import { FriendList } from './FriendList';
+import { TransactionHistory } from './TransactionHistory';
+
 import friends from '../data/friends.json';
 import user from '../data/user.json';
 import data from '../data/data.json';
+import transactions from '../data/transactions.json';
 
 const App = () => {
   return (
@@ -17,6 +20,7 @@ const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
